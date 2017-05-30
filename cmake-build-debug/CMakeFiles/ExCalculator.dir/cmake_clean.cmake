97dc43c6476ac219720b07271da23d02c806b1df
c0desBym3ta <kalimetasploiter@gmail.com>
2017-05-31 00:59:06 +0200

    Commiting additional files.

---
+++ cmake-build-debug/CMakeFiles/ExCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExCalculator.dir/main.cpp.o"
+  "ExCalculator.pdb"
+  "ExCalculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
