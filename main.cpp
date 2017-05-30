bfc2a4d7248e196ea25da3e6bb99f2f5525ff34d
c0desBym3ta <kalimetasploiter@gmail.com>
2017-05-31 01:20:15 +0200

    Adding if to select operation.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,33 @@ void line();
 
 int main() {
     float num1, num2;
+    std::string op;
     std::cout << "Insert first number: ";
     std::cin >> num1;
     std::cout << "Insert second number: ";
     std::cin >> num2;
     line();
 
-    std::cout << "Addition result: " << addition(num1,num2) << std::endl;
-    std::cout << "Subtraction reslut: " << subtraction(num1,num2) << std::endl;
-    std::cout << "Moltiplication result: " << moltiplication(num1,num2) << std::endl;
-    std::cout << "Divistion result: " << division(num1, num2) << std::endl;
+
+    std::cout << "\t\tSELECT SYMBOL of OPERATION" << std::endl;
+    std::cout << "\t\t....................." << std::endl;
+    std::cout <<"At the moment we have 4 available operations ()." << std::endl;
+    std::cout <<"1)Addition (+).\n2)Subtraction (-).\n3)Moltiplication (*).\n4)Division (/)."<<std::endl;
+    line();
+
+    std::cout << "SELECTION: \t";
+    std::cin >> op;
+    line();
+
+    if(op == "+")
+        std::cout << "Addition result is: " << addition(num1,num2) << std::endl;
+    else if(op == "-")
+        std::cout << "Subtraction result is : " << subtraction(num1,num2) << std::endl;
+    else if (op == "*")
+        std::cout << "Moltiplication result is : " << moltiplication(num1,num2) << std::endl;
+    else if(op == "/")
+        std::cout << "Divistion result is: " << division(num1,num2) << std::endl;
+
     return 0;
 }
 
